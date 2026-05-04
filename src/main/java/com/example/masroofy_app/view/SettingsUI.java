abbf861c101746410d19d638f26b12a31026c9c7
Abdallah Ahmed <abdallah.ab1492006@gmail.com>
2026-05-04 14:27:37 +0300

    Update SettingsUI.java

--- src/main/java/com/example/masroofy_app/view/SettingsUI.java
+++ src/main/java/com/example/masroofy_app/view/SettingsUI.java
@@ -1,26 +1,35 @@
 package com.example.masroofy_app.view;
 
+import com.example.masroofy_app.model.DatabaseHelper;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
 
 public class SettingsUI {
 
     @FXML
-    private javafx.scene.control.CheckBox privacyCheck;
+    private CheckBox privacyCheck;
+
+    @FXML
+    public void initialize() {
+        boolean isEnabled = DatabaseHelper.getInstance().isPrivacyEnabled();
+        privacyCheck.setSelected(isEnabled);
+
+        privacyCheck.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            DatabaseHelper.getInstance().setPrivacyEnabled(newValue);
+            System.out.println("Privacy setting saved: " + (newValue ? "ON" : "OFF"));
+        });
+    }
 
     @FXML
     public void goBack() {
         try {
-            Parent root = FXMLLoader.load(
-                    getClass().getResource("/view/HistoryUI.fxml")
-            );
-
+            Parent root = FXMLLoader.load(getClass().getResource("/view/DashboardUI.fxml"));
             Stage stage = (Stage) privacyCheck.getScene().getWindow();
             stage.setScene(new Scene(root));
-
         } catch (Exception e) {
             e.printStackTrace();
         }
